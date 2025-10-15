383c8366bde3d35e30afdaaf256d7a6a582bea70
Artu211 <160549068+Artu211@users.noreply.github.com>
2025-10-15 11:52:36 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/nawigacja/MainActivity.java
+++ app/src/main/java/com/example/nawigacja/MainActivity.java
@@ -14,17 +14,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button openSecondActivityButton = findViewById(R.id.openSecondActivityButton);
+
         Button phone = findViewById(R.id.phone);
         Button map = findViewById(R.id.map);
         Button mail = findViewById(R.id.mail);
         Button gallery = findViewById(R.id.gallery);
+        Button openFirstActivityButton = findViewById(R.id.openFirstActivityButton);
+        Button openSecondActivityButton = findViewById(R.id.openSecondActivityButton);
+        Button openThirdActivityButton = findViewById(R.id.openThirdActivityButton);
+
+        openFirstActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+                // 2. Uruchomienie Aktywności
+                startActivity(intent);
+            }
+        });
 
         openSecondActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                // 2. Uruchomienie Aktywności
+                startActivity(intent);
+            }
+        });
+
+        openThirdActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
 
                 // 2. Uruchomienie Aktywności
                 startActivity(intent);
