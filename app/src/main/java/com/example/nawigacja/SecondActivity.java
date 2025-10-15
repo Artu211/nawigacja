97784db5ec875e5eb3110d02ff23baf7a41ec59c
Artu211 <160549068+Artu211@users.noreply.github.com>
2025-10-15 11:52:54 +0200

    Update SecondActivity.java

--- app/src/main/java/com/example/nawigacja/SecondActivity.java
+++ app/src/main/java/com/example/nawigacja/SecondActivity.java
@@ -13,13 +13,36 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button openSecondActivityButton = findViewById(R.id.openThirdActivityButton);
+        Button openFirstActivityButton = findViewById(R.id.openFirstActivityButton);
+        Button openSecondActivityButton = findViewById(R.id.openSecondActivityButton);
+        Button openThirdActivityButton = findViewById(R.id.openThirdActivityButton);
+
+        openFirstActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+
+                // 2. Uruchomienie Aktywności
+                startActivity(intent);
+            }
+        });
 
         openSecondActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 1. Stworzenie intencji jawnej
-                // Podajemy kontekst (this) i klasę docelowej Aktywności
+
+                Intent intent = new Intent(SecondActivity.this, SecondActivity.class);
+
+                // 2. Uruchomienie Aktywności
+                startActivity(intent);
+            }
+        });
+
+        openThirdActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
 
                 // 2. Uruchomienie Aktywności
